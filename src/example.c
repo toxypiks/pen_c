53e31292a3a05f0af299ce6a15317878b3a13726
toxypiks <HannaHinkelbein89@gmx.de>
2025-03-07 20:49:51 +0100

    Draw checkboard

--- src/example.c
+++ src/example.c
@@ -25,10 +25,28 @@ void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_heigh
 
 static uint32_t pixels[HEIGHT*WIDTH];
 
+#define COLS 8
+#define ROWS 6
+#define CELL_WIDTH (WIDTH/COLS)
+#define CELL_HEIGHT (HEIGHT/ROWS)
+
 int main(void)
 {
     olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
-    olivec_fill_rect(pixels, WIDTH, HEIGHT, 0, 0, 50, 30, 0xFF2020FF);
+
+    //checkboard
+    for (int y =0; y < ROWS; ++y) {
+        for (int x = 0; x < COLS; ++x) {
+            uint32_t color;
+            if ((x + y)%2 == 0) {
+                color = 0xFF0000FF;
+            } else {
+                color = 0xFF00FF00;
+            }
+            olivec_fill_rect(pixels, WIDTH, HEIGHT, x*CELL_WIDTH, y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
+        }
+    }
+
     const char *file_path = "../output.ppm";
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
     if (err) {
