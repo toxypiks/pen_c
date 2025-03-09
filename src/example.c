406962967df27f626b1f7d40577aaec53d85e2fd
toxypiks <HannaHinkelbein89@gmx.de>
2025-03-09 17:14:25 +0100

    Implement circle checkboad WIP

--- src/example.c
+++ src/example.c
@@ -68,7 +68,13 @@ bool circle_example(void)
 {
     olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
-    olivec_fill_circle(pixels, WIDTH, HEIGHT, WIDTH/2, HEIGHT/2, 100, FOREGROUND_COLOR);
+    for (int y = 0; y < ROWS; ++y) {
+        for (int x = 0; x < COLS; ++x) {
+            size_t radius = CELL_WIDTH;
+            if (CELL_HEIGHT < radius) radius = CELL_HEIGHT;
+            olivec_fill_circle(pixels, WIDTH, HEIGHT, x*CELL_WIDTH + CELL_WIDTH/2, y*CELL_HEIGHT + CELL_HEIGHT/2, radius/2, FOREGROUND_COLOR);
+        }
+    }
 
     const char *file_path = "../circle.ppm";
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
