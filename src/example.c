2930452fabd90abc0fb25d7787de686cdd8ea8f7
toxypiks <HannaHinkelbein89@gmx.de>
2025-03-17 12:06:12 +0100

    Draw diagonal line with draw_line function

--- src/example.c
+++ src/example.c
@@ -18,6 +18,53 @@
 
 static uint32_t pixels[HEIGHT*WIDTH];
 
+void swap_int(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void olivec_draw_line(uint32_t *pixels, size_t pixel_width, size_t pixel_height, int x1, int y1, int x2, int y2, uint32_t color)
+{
+    // y = k*x + c
+    //
+    // y1 = k*x1 + c
+    // y2 = k*x2 + c
+    //
+    // y1 - k*x1 = c
+    // y2        = k*x2 + y1 - k*x1
+    // y2        = k*(x2 - x1) + y1
+    // y2 - y1 = k*(x2 - x1)
+    // (y2 -y1)/(x2 - x1) = k
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    if (dx != 0) {
+        int c = y1 - dy*x1/dx;
+
+        if (x1 > x2) swap_int(&x1, &x2);
+        for (int x = x1; x <= x2; ++x) {
+            int y = dy*x/dx + c;
+            if (0 <= x && x < (int)pixel_width) {
+                pixels[y*pixel_width + x] = color;
+            }
+        }
+    } else {
+        //dx = 0 -> vertical line
+        int x = x1;
+        if (0 <= x && x < (int)pixel_width) {
+            if (y1 < y2) swap_int(&y1, &y2);
+            for (int y = y1; y <= y2; ++y ) {
+                if (0 <= y && y < (int)pixel_height) {
+                    pixels[y*pixel_width + x] = color;
+                }
+            }
+        }
+    }
+}
+
 bool checker_example(void)
 {
     olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
@@ -76,9 +123,27 @@ bool circle_example(void)
     return true;
 }
 
+bool lines_example(void)
+{
+    olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT,
+                          0, 0, WIDTH, HEIGHT,
+                          FOREGROUND_COLOR);
+
+    const char *file_path = "../lines.ppm";
+    Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+    if (err) {
+        fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
+        return false;
+    }
+    return true;
+}
+
 int main(void)
 {
     if (!checker_example()) return -1;
     if (!circle_example()) return -1;
+    if (!lines_example()) return -1;
     return 0;
 }
